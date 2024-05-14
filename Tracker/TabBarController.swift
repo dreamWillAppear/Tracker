7b98dc67124cd1d0f7280cd124196ee7f2d56020
dreamlogin <dreamlogin@internet.ru>
2024-05-15 00:18:29 +0300

    Подготовка проекта Tracker

---
+++ Tracker/TabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarController.swift
+//  Tracker
+//
+//  Created by Shestakov Bogdan on 15.05.2024.
+//
+
+import Foundation
--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  Tracker
-//
-//  Created by dreamlogin on 13.05.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
     }
-
-
 }
 
