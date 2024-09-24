95802f69a6b6fd1a73a619bb66a86ea9d4332aa2
dreamWillAppear <dreamlogin@internet.ru>
2024-09-24 19:56:05 +0300

    Подготовлен UI для экрана статистики

---
+++ Tracker/ViewControllers/StatisticsViewController/StatisticsCounterView.swift
@@ -0,0 +1,106 @@
+import UIKit
+import SnapKit
+
+class StatisticsCounter: UIView {
+    
+    var counter: Int
+    
+    var counterName: String
+    
+    private lazy var counterLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.text = String(counter)
+        let textSize = (label.text?.count ?? 0) < 13 ? 34 : 34/2 as CGFloat //на случай, если значение будет слишком большим
+        label.font = .systemFont(ofSize: textSize, weight: .bold)
+        label.textAlignment  = .left
+        return label
+    }()
+    
+    private lazy var counterNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.text = counterName
+        label.font = .systemFont(ofSize: 12, weight: .medium)
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var labelsStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .equalSpacing
+        stack.layoutMargins = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+        stack.isLayoutMarginsRelativeArrangement = true 
+        return stack
+    }()
+    
+    
+    
+    
+    // Инициализация
+    init(counter: Int, counterName: String) {
+        self.counter = counter
+        self.counterName = counterName
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Метод для настройки представления
+    private func setupView() {
+        layer.cornerRadius = 16
+        layer.masksToBounds = true
+        
+        // Добавляем подвиды
+        [counterLabel, counterNameLabel].forEach {
+            labelsStackView.addArrangedSubview($0)
+        }
+        
+        addSubview(labelsStackView)
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        labelsStackView.snp.makeConstraints { make in
+            make.leading.trailing.top.bottom.equalToSuperview()
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addGradientBorder(width: 1)
+    }
+}
+
+extension UIView {
+    func addGradientBorder(width: CGFloat = 1) {
+        let gradientLayer = CAGradientLayer()
+        //по rgb из фигмы
+        gradientLayer.colors = [
+            UIColor(red: 0.0, green: 0.482, blue: 0.98, alpha: 1.0).cgColor,
+            UIColor(red: 0.275, green: 0.902, blue: 0.616, alpha: 1.0).cgColor,
+            UIColor(red: 0.992, green: 0.298, blue: 0.286, alpha: 1.0).cgColor
+        ]
+        
+        gradientLayer.startPoint = CGPoint(x: 1, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.frame = bounds
+        
+        let borderLayer = CAShapeLayer()
+        let insetBounds = bounds.insetBy(dx: width, dy: width)
+        borderLayer.path = UIBezierPath(roundedRect: insetBounds, cornerRadius: layer.cornerRadius).cgPath
+        borderLayer.lineWidth = width
+        borderLayer.fillColor = UIColor.clear.cgColor
+        borderLayer.strokeColor = UIColor.black.cgColor
+        gradientLayer.mask = borderLayer
+        
+        layer.addSublayer(gradientLayer)
+    }
+}
+
+
--- Tracker/ViewControllers/StatisticsViewController/StatisticsViewController.swift
+++ Tracker/ViewControllers/StatisticsViewController/StatisticsViewController.swift
@@ -18,6 +18,7 @@ final class StatisticsViewController: UIViewController {
         view.axis = .vertical
         view.alignment = .center
         view.spacing = 8
+        view.isHidden = true
         return view
     }()
     
@@ -27,18 +28,31 @@ final class StatisticsViewController: UIViewController {
         return view
     }()
     
-    private var noStatisticsLabel: UILabel = {
+    private lazy var noStatisticsLabel: UILabel = {
         let label = UILabel()
         label.text = "Анализировать пока нечего"
         label.font = .systemFont(ofSize: 12)
         return label
     }()
     
+    private lazy var countersStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 12
+        return stack
+    }()
+    
+    private lazy var bestPeriod = StatisticsCounter(counter: 123123, counterName: "Лучший период")
+    private lazy var perfectDays = StatisticsCounter(counter: 23, counterName: "Идеальные дни")
+    private lazy var completedTrackers = StatisticsCounter(counter: 1, counterName: "Трекеров завершено")
+    private lazy var averageCount = StatisticsCounter(counter: 2333333945999999955, counterName: "Среднее значение")
+    
     // MARK: - Public Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setUI()
     }
     
@@ -48,6 +62,7 @@ final class StatisticsViewController: UIViewController {
         view.backgroundColor = .trackerWhite
         
         [mainLabel,
+         countersStackView,
          noStatisticsStackView].forEach {
             view.addSubview($0)
         }
@@ -55,6 +70,10 @@ final class StatisticsViewController: UIViewController {
         [noStatisticsImageView,
          noStatisticsLabel].forEach {
             noStatisticsStackView.addArrangedSubview($0)
+        }
+        
+        [bestPeriod, perfectDays, completedTrackers, averageCount].forEach {
+            countersStackView.addArrangedSubview($0)
         }
         
         setConstraints()
@@ -76,6 +95,33 @@ final class StatisticsViewController: UIViewController {
         noStatisticsImageView.snp.makeConstraints { make in
             make.width.height.equalTo(80)
         }
+        
+        countersStackView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(mainLabel.snp.bottom).inset(-84)
+            make.height.equalTo(getCountersStackViewHeight()) //высотка вьюх + высота отсупов по макету
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
     }
     
+    private func getCountersStackViewHeight() -> CGFloat {
+        let visibleViews = countersStackView.visibleViewsCount()
+        guard visibleViews  > 1 else { return 90 }
+        
+        return CGFloat((visibleViews * 90) + (12 * visibleViews - 1)) 
+    }
+    
+    
+}
+    
+extension UIStackView {
+     func visibleViewsCount() -> Int {
+        var visibleViewCount = 0
+        self.arrangedSubviews.forEach {
+            if !$0.isHidden {
+                visibleViewCount += 1
+            }
+        }
+        return visibleViewCount
+    }
 }
